226cb680c6a86540c999e1689492718a92d619f9
silvon <silvonli@gmail.com>
2012-09-22 23:35:33 +0800

    Initial Commit

---
+++ AnimalStudy/ios/AppController.h
@@ -0,0 +1,17 @@
+//
+//  AnimalStudyAppController.h
+//  AnimalStudy
+//
+//  Created by sumomoxiaowen on 12-9-22.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@end
+
